b91390aff5cb9bd978c8c8ed09045d6730f28009
Erigers <erigersmamoci@gmail.com>
2016-09-23 12:18:55 +0200

    first phase

---
+++ animation.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.play-holder').addClass('animate-logo');
+  setTimeout(function(){
+    $('.play-holder').addClass('rotate-logo');
+  }, 1000);
+  setTimeout(function(){
+    $('.text-holder').addClass('tobo');
+  }, 1600);
+});

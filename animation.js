960c4a2fb54f62d203ff0171dc6b7a362f917821
erigers <erigersmamoci@gmail.com>
2016-11-15 20:29:48 +0100

    no disappear

--- animation.js
+++ animation.js
@@ -5,41 +5,41 @@ $(document).ready(function() {
         $(".play-holder").addClass("rotate-logo")
     }, 1500), setTimeout(function() {
         $(".line-shadow").addClass("top-border-display")
-    }, 2100), setTimeout(function() {
-        $(".main-frame").addClass("move-right")
-    }, 2300), setTimeout(function() {
-        $(".text-shadow").addClass("tesha-reducer")
     }, 2600), setTimeout(function() {
+        $(".main-frame").addClass("move-right")
+    }, 2800), setTimeout(function() {
+        $(".text-shadow").addClass("tesha-reducer")
+    }, 3100), setTimeout(function() {
         $(".text-holder").addClass("teho-up")
-    }, 2700), setTimeout(function() {
+    }, 3200), setTimeout(function() {
         $(".text-holder").addClass("smaller-font")
-    }, 3e3), setTimeout(function() {
-        $(".play-holder svg").addClass("rotate-a-third"), $(".main-frame").removeClass("move-right").css("margin-left", "-9px").css("margin-top", "78px")
-    }, 3e3), setTimeout(function() {
-        $(".line-shadow2").addClass("top-border-display")
-    }, 3300), setTimeout(function() {
-        $(".main-frame").css("margin-left","25px")
     }, 3500), setTimeout(function() {
-        $(".text-shadow2").addClass("tesha-reducer")
+        $(".play-holder svg").addClass("rotate-a-third"), $(".main-frame").removeClass("move-right").css("margin-left", "-9px").css("margin-top", "78px")
+    }, 3500), setTimeout(function() {
+        $(".line-shadow2").addClass("top-border-display")
     }, 3800), setTimeout(function() {
+        $(".main-frame").css("margin-left","25px")
+    }, 4000), setTimeout(function() {
+        $(".text-shadow2").addClass("tesha-reducer")
+    }, 4300), setTimeout(function() {
         $(".text-holder2").addClass("teho-up")
-    }, 3900), setTimeout(function() {
+    }, 4400), setTimeout(function() {
         $(".text-holder2").addClass("smaller-font")
-    }, 4200), setTimeout(function() {
+    }, 4700), setTimeout(function() {
         $(".play-holder svg").addClass("rotate-another-third"), $(".main-frame").removeClass("move-right").css("margin-left", "-9px").css("margin-top", "82px")
-    }, 4200), setTimeout(function() {
+    }, 4700), setTimeout(function() {
         $(".line-shadow3").addClass("top-border-display")
-    }, 4500), setTimeout(function() {
+    }, 5000), setTimeout(function() {
         $(".main-frame").css("margin-left", "25px")
-    }, 4800), setTimeout(function() {
+    }, 5300), setTimeout(function() {
         $(".text-shadow3").addClass("tesha-reducer")
-    }, 5e3), setTimeout(function() {
+    }, 5500), setTimeout(function() {
         $(".text-holder3").addClass("teho-up")
-    }, 5100), setTimeout(function() {
+    }, 5600), setTimeout(function() {
         $(".text-holder3").addClass("smaller-font")
-    }, 5400), setTimeout(function() {
+    }, 5900), setTimeout(function() {
         $(".play-holder").removeClass("animate-logo").addClass("back-2-init")
-    }, 6e3), setTimeout(function() {
-        $(".play-holder").addClass("zero-zero")
-    }, 6800)
+    }, 6500), setTimeout(function() {
+        $(".play-holder").addClass("zero-zero");
+    }, 7300)
 });
